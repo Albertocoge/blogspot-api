a7a4e6663e18eea6675da7093382d686a4094638
Alberto Codoñer Gea <alberto.cg2197@gmail.com>
2025-10-17 15:38:43 +0200

    Cambios locales antes del cherry-pick

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const app = express();
 
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
-//app.use(cors({credentials: true, origin: process.env.CLIENT_URL || "http://localhost:5173"  
-//}))
+app.use(cors({credentials: true, origin: process.env.CLIENT_URL || "http://localhost:5173"  
+}))
 app.use(upload())
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
@@ -23,5 +23,4 @@ app.use('/api/posts' , postRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
-connect(process.env.MONGO_URI).then(app.listen(process.env.PORT || 5000 , () => console.log(`Server running on port ${process.env.PORT}`))).catch(error => {console.log(error)})
-
+connect(process.env.MONGO_URI).then(app.listen(process.env.PORT || 3000 , () => console.log(`Server running on port ${process.env.PORT}`))).catch(error => {console.log(error)})
